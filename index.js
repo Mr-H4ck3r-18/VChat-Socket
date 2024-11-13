2d255a231b7bcb7e4b2306bc279681f01574274b
Sasank Ramaraju <sasankramaraju@Sasanks-MacBook-Air.local>
2024-11-14 01:07:16 +0530

    VChat Web socket

---
+++ index.js
@@ -0,0 +1,48 @@
+import { Server } from 'socket.io';
+
+const io = new Server(9000, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+let users = [];
+
+const addUser = (userData, socketId) => {
+    const existingUser = users.find(user => user.sub === userData.sub);
+    if (existingUser) {
+        existingUser.socketId = socketId;
+    } else {
+        users.push({ ...userData, socketId });
+    }
+};
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+};
+
+const getUser = (data) => {
+    const userDetails = users.find(user => user.sub === data);
+    // console.log(userDetails)
+    return userDetails;
+}
+
+io.on('connection', (socket) => {
+    console.log("User connected");
+
+    socket.on("addUser", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    socket.on("sendMessage", data => {
+        const user = getUser(data.receiverId);
+        io.to(user.socketId).emit('getMessage', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+});
