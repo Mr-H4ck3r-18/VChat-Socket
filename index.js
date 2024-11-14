2a2a945b674e3259985095e714468aed53c80cde
Sasank Ramaraju <sasankramaraju@Sasanks-MacBook-Air.local>
2024-11-14 17:21:11 +0530

    VChat Web socket final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: "https://vchat-socket.onrender.com"
+        origin: "http://localhost:3000"
     }
 });
 
